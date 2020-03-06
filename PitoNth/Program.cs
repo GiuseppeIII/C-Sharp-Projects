e5d595fc1fb705d0792b21581a0b430612136114
Joseph Babbo <jbabbo2@illinois.edu>
2020-03-05 23:00:10 -0600

    PitoNth setup testing

---
+++ PitoNth/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PitoNth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

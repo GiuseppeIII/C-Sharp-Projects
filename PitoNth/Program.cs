c6fb7e9fd713f30653c0b22b2fa3e24096676e32
Joseph Babbo <jbabbo2@illinois.edu>
2020-03-06 00:34:58 -0600

    eToNth Complete

--- PitoNth/Program.cs
+++ PitoNth/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 
-namespace PitoNth
+namespace eToNth
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const double e = Math.E;
+            string digits;
+            bool goodInput = true;
+            bool goAgain = false;
+
+            //test input
+            do
+            {
+                do
+                {
+                    goodInput = true;
+                    Console.WriteLine("How many digits of e do you want to see? [1, 15]");
+                    digits = Console.ReadLine();
+                    try
+                    {
+                        Convert.ToInt16(digits);
+                        if (Convert.ToInt16(digits) < 1 || Convert.ToInt16(digits) > 15)
+                        {
+                            Console.WriteLine("Please enter an integer value between 1 and 15");
+                            goodInput = false;
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Please enter an integer value between 1 and 15");
+                        goodInput = false;
+                    }
+                } while (goodInput == false);
+
+                //
+                Console.WriteLine(Math.Round(e, Convert.ToInt16(digits) - 1));
+                Console.WriteLine("Do you want to go again? (y/n)");
+                if (Console.ReadLine().ToLower() == "y")
+                {
+                    goAgain = true;
+                }
+                else
+                {
+                    goAgain = false;
+                }
+            } while (goAgain == true);
+
         }
     }
 }

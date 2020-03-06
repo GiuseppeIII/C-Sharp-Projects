3e9ecf2afb05166bf31325197458ab3a463ef647
jbabbo2 <jbabbo2@illinois.edu>
2020-03-06 02:13:17 -0600

    Console Coin Flip complete

--- ConsoleCoinFlip/Program.cs
+++ ConsoleCoinFlip/Program.cs
@@ -6,7 +6,30 @@ namespace ConsoleCoinFlip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+						Random coin = new Random();
+						int heads=0;
+						int tails=0;
+            Console.WriteLine("Welcome to the Console Coint Flip simulator! \nTo flip a coin press any key, to see this sessions stats press enter, to stop press escape.");
+						while (true){
+							System.ConsoleKey Key = Console.ReadKey().Key;
+							if (Key == ConsoleKey.Enter){
+								Console.WriteLine("There have been {0} heads and {1} tails!", heads, tails);
+							}
+							else if (Key == ConsoleKey.Escape){
+								break;
+							}
+							else{
+								int flip = coin.Next(0,2);
+								if (flip==0){
+									Console.WriteLine("\nYou got heads!");
+									heads++;
+								}
+								else{
+									Console.WriteLine("\nYou got tails!");
+									tails++;
+								}
+							}
+						}
         }
     }
 }
